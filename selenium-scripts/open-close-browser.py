6a34661336252eb61039e282d74ab6dd4fdabdd4
Kavana Kabbahally Mahadevaswamy <kavanamswamy20@gmail.com>
2025-11-15 11:02:09 +0400

    Delete selenium-scripts/open-close-browser.py
    Commit message was incorrect

--- selenium-scripts/open-close-browser.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver  #imports required modules - to control browser
-from selenium.webdriver.chrome.service import Service   #to manage chromedriver process
-service=Service("C:/Users/surya/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe")  #creates service object
-driver = webdriver.Chrome(service=service)  #initialize chrome using service object
-
-driver.get("https://www.google.com")    #nabigates to google
-title=driver.title #gets the title name
-print(f"Page title is {title}") #prints the title
-driver.quit()   #closes the browser
